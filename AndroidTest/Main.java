408ac41265f4450be55d9c1db26442dd11884079
Jonathan Clark <clarkrjonathan@gmail.com>
2025-02-27 11:26:57 -0600

    Additional organizing
    Signed-off-by: Jonathan Clark <clarkrjonathan@gmail.com>

--- AndroidTest/Main.java
+++ AndroidTest/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         //Compilation Check:
         System.out.println("Hello World");
+        System.out.println("Compilation Check");
     }
 }
